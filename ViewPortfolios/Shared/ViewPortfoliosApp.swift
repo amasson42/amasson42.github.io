ca96d21b6cc352bd68c9b4ea6abf51cece8b5d33
amasson42 <giantwow3896@gmail.com>
2021-09-09 11:39:06 +0200

    SwiftUI - Add the swiftUI project for the portfolios

---
+++ ViewPortfolios/Shared/ViewPortfoliosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewPortfoliosApp.swift
+//  Shared
+//
+//  Created by Giantwow on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewPortfoliosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
